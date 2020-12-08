2bf71e870bd828b4247ec671f632d2c6b1f60627
Lusifer <70184956+lusifernoob@users.noreply.github.com>
2020-12-08 16:12:40 +0530

    Update __main__.py

--- Elizabeth/__main__.py
+++ Elizabeth/__main__.py
@@ -40,7 +40,7 @@ from Elizabeth.modules.helper_funcs.alternate import typing_action
 
 PM_START_TEXT = f"""
 `Hi..Welcome` 👀
-`I'm` [ELIZABETH🍀](https://telegra.ph/file/281aaa468756db0769df0.jpg)
+`I'm` [✔️ NIKITA🍀](https://telegra.ph/file/281aaa468756db0769df0.jpg)
 `I'm here to help you manage your groups.. Click Help button to find out more about how to use me to my full potential..`
 """
 
@@ -53,9 +53,9 @@ buttons += [[InlineKeyboardButton(text="🌍 ADD ME TO YOUR GROUP 🌍",
 ]]
 
 buttons += [[InlineKeyboardButton(text="💾SOURCE CODE",
-                                  url="https://github.com/P-RINC-E/ELIZABETH"),
+                                  url="⚠️ERROR 🚫 404🚫NOT FOUND TRY AGAIN LATER ‼️☢️ ⚠️"),
              InlineKeyboardButton(text="🌳SUPPORT",
-                                  url="https://t.me/ELIZABETH_SUPPORT"),
+                                  url="https://t.me/OMG_info"),
 ]]
 
 buttons += [[InlineKeyboardButton(text="🔒 CLOSE THE MENU 🔒",
@@ -63,7 +63,7 @@ buttons += [[InlineKeyboardButton(text="🔒 CLOSE THE MENU 🔒",
 
 
 HELP_STRINGS = f"""
-`Hi.. I'm` [ELIZABETH🍀](https://telegra.ph/file/281aaa468756db0769df0.jpg)
+`Hi.. I'm` [✔️NIKITA🍀](https://telegra.ph/file/6cf3dfe51c66e1b823725.jpg)
 `Click on the buttons below to get documentation about specific modules..`"""
 
 
